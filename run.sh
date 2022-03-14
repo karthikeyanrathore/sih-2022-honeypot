b3c0487e24bfe106a06ead0583fef87a4a6c0329
karthikeyan <vagrant@batman.local>
2022-03-15 01:05:16 +0530

    run: mac m1 only

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [[ "$OSTYPE" == "darwin"* ]]  && [[ $LOCAL == 1 ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]  && [[ $LOCAL == 1 ]] && [[ $USER == "vagrant" ]]; then
   source ~/.bash_profile
   conda activate sih
   chmod +x build/app.py
   cd build
-  sudo ./app.py
+  ./app.py
   cd ..
 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]  && [[ $PRO == 1 ]]; then
