59a4b31061303ff660b37b4fa151568828ab128e
karthikeyan_01 <karthikerathore@gmail.com>
2022-05-13 10:21:30 +0530

    bash script runs for all

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-if [[ "$OSTYPE" == "darwin"* ]]  && [[ $LOCAL == 1 ]] && [[ $USER == "vagrant" ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]  && [[ $LOCAL == 1 ]];  then
   source ~/.bash_profile
   conda activate sih
   chmod +x build/app.py
   cd build
+  if [ ! -d "WAV" ]; then
+    mkdir WAV
+  fi
   ./app.py
   cd ..
 
